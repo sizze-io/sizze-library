dbc1b3d2c264600b1e22892aaa627a523df7982e
Sergei Kurakov <sergei@MacBook-Pro-6.local>
2024-02-13 11:21:05 +0300

    fix(rollup.config.js): update style

--- src/Sznavbar.tsx
+++ src/Sznavbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavBar1 } from '../devlink/NavBar1';
-import '@/devlink/global.css'
+import '../devlink/global.css'
 
 export function Sznavbar() {
   return <NavBar1 />;
